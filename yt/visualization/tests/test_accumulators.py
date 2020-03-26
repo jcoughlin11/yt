d454bf205a41f06bf1fb62a1d8f597c4686c1e32
Jared <jcoughl3@illinois.edu>
2020-03-26 17:13:30 -0500

    Added the npts tests for accumulators.

--- yt/visualization/tests/test_accumulators.py
+++ yt/visualization/tests/test_accumulators.py
@@ -16,7 +16,7 @@ def _generate_fake_path():
     Samples N (x,y,z) points along y^2=x and z^3=x from (0.1, 0.1, 0.1) ->
     (0.9,0.9^(0.5), 0.9, (1./3.)).
     """
-    N = 50
+    N = 10
     x = np.linspace(0.1, 0.9, N)
     y = np.linspace(0.1, np.sqrt(0.9), N)
     z = np.linspace(0.1, np.cbrt(0.9), N)
@@ -43,7 +43,13 @@ def test_two_pts_diff_cell_same_node_scalar():
     Integrates the density field between two points that are in the same
     node but in different cells.
     """
-    pass
+    ds = data_dir_load(g30)
+    path = np.array([[.16, .14, .44], [.22, .4, .8]])
+    accumulator = Accumulators([path], ds)
+    field = [('enzo', 'Density')]
+    accumulator.accumulate(field, is_vector=False)
+    answer = np.array([.06582006])
+    np.testing.assert_array_almost_equal(accumulator.accum, answer)
 
 @requires_ds(g30, big_data=True)
 def test_two_pts_diff_nodes_scalar():
@@ -68,8 +74,23 @@ def test_npts_scalar():
 
     NOTE: The answer will depend on the number of points used to define
     the path, so if that changes, update the answer accordingly!
+    The current answer is for N = 10
     """
-    pass
+    ds = data_dir_load(g30)
+    path = _generate_path()
+    accumulator = Accumulators([path], ds)
+    field = [('enzo', 'Density')]
+    accumulator.accumulate(field, is_vector=False)
+    answer = np.array([0.02904837,
+                0.05809688,
+                0.08693819,
+                0.11599762,
+                10.42846079,
+                11.73346686,
+                11.76252416,
+                11.79157306,
+                11.82062148])
+    np.testing.assert_array_almost_equal(accumulator.accum, answer)
 
 @requires_ds(g30, big_data=True)
 def test_two_pts_same_cell_vector():
@@ -91,7 +112,13 @@ def test_two_pts_diff_cell_same_node_vector():
     Integrates the velocity field between two points that are in the same
     node but in different cells.
     """
-    pass
+    ds = data_dir_load(g30)
+    path = np.array([[.16, .14, .44], [.22, .4, .8]])
+    field = [('enzo', 'x-velocity'), ('enzo', 'y-velocity'), ('enzo', 'z-velocity')]
+    accumulator = Accumulators([path], ds)
+    accumulator.accumulate(field, is_vector=True)
+    answer = np.array([0.0264723435])
+    np.testing.assert_array_almost_equal(accumulator.accum, answer)
 
 @requires_ds(g30, big_data=True)
 def test_two_pts_diff_nodes_vector():
@@ -116,5 +143,20 @@ def test_npts_vector():
 
     NOTE: The answer will depend on the number of points used to define
     the path, so if that changes, update the answer accordingly!
+    The current answer is for N = 10
     """
-    pass
+    ds = data_dir_load(g30)
+    path = _generate_path()
+    field = [('enzo', 'x-velocity'), ('enzo', 'y-velocity'), ('enzo', 'z-velocity')]
+    accumulator = Accumulators([path], ds)
+    accumulator.accumulate(field, is_vector=True)
+    answer = np.array([0.01137986,
+                0.03029503,
+                0.06936842,
+                0.19213309,
+                0.96494272,
+                0.63585663,
+                0.56452914,
+                0.5376544,
+                0.52296114])
+    np.testing.assert_array_almost_equal(accumulator.accum, answer)
